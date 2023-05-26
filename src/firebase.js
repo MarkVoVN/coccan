9a95efe44f8e693d65281b57115c556e150af78a
MarkVo_Mac <markvo_mac@MarkVo-Macs-MacBook-Pro.local>
2023-05-26 14:13:22 +0700

    Create (login, firebase): Initialize Firebase, Log in with Google Popup FE

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAF_B_F97-BKfjP9iBdqY_-lQajGK7_3H4",
+  authDomain: "coccan-b34bb.firebaseapp.com",
+  projectId: "coccan-b34bb",
+  storageBucket: "coccan-b34bb.appspot.com",
+  messagingSenderId: "480571589199",
+  appId: "1:480571589199:web:d415350aedc55ec01aa03a",
+  measurementId: "G-P080ZJXLCK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
