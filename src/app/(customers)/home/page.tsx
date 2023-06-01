02fe53709e51cfb04755e256f0fc43736a22e2d7
MarkVo_Mac <markvo_mac@192.168.2.8>
2023-06-01 23:12:11 +0700

    added cta, session selector, category list, product list to Homepage

--- src/app/(customers)/home/page.tsx
+++ src/app/(customers)/home/page.tsx
@@ -1,13 +1,49 @@
-import Link from 'next/link'
+import CallToActionSection from "./CallToActionSection"
+import SessionSeletorSection from "./SessionSeletorSection"
+import CategorySeletorSection from "./CategorySeletorSection"
+import ProductByCategorySection from "./ProductByCategorySection"
+import './style.scss'
+
 
 export default function Home() {
 
+  const categoryList = [
+    {
+      categoryId : 0,
+      categoryIconUrl : 'rice.svg',
+      name: 'Rice',
+    },
+    {
+      categoryId : 1,
+      categoryIconUrl : 'bread.svg',
+      name: 'Bread',
+    },
+    {
+      categoryId : 2,
+      categoryIconUrl : 'drink.svg',
+      name: 'Drink',
+    },
+    {
+      categoryId : 3,
+      categoryIconUrl : 'snack.svg',
+      name: 'Snack',
+    },
+    {
+      categoryId : 4,
+      categoryIconUrl : 'others.svg',
+      name: 'Others',
+    },
+  ]
+
   return (
     <>
-      <div>Home</div>
-      <Link href='/login'>Log In</Link>
-      <Link href='/profile'>Profile</Link>
-    
+      <CallToActionSection></CallToActionSection>
+      <SessionSeletorSection></SessionSeletorSection>
+      <CategorySeletorSection categoryList={categoryList}></CategorySeletorSection>
+      {categoryList.map((category) => (
+        <ProductByCategorySection key={category.categoryId} category={category}></ProductByCategorySection>
+      ))}
+      
     </>
   )
 }
