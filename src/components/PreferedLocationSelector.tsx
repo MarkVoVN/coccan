e8584449b67796b429242c0e6033cb8a93f539f3
Minh Khoi <62336255+MarkVoVN@users.noreply.github.com>
2023-06-19 18:39:19 +0700

    User profile (#15)
    * update 404 svg file name
    
    * misc
    
    * Added RTK
    
    * Added redux tool kit
    
    * finished setting up RTK
    
    * integrate RTK into checking for order info, user, and cart
    
    * implement saving state to session for storage
    
    * fix type error
    
    * added user profile, dropdown menu in navbar, cart page, migrate firebase config cred to .env
    
    * merging
    
    * type error fix

---
+++ src/components/PreferedLocationSelector.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import {
+  updatePreferedLocation,
+  useAppSelector,
+} from "@/app/GlobalRedux/Features/userSlice";
+import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { useDispatch } from "react-redux";
+
+function PreferedLocationSelector() {
+  //TODO: Get list of locations from Redux populate Menu
+  const userPreferedLocationId = useAppSelector(
+    (state) => state.user.value.preferedLocationId
+  );
+  const dispatch = useDispatch();
+  const handleChange = (e: SelectChangeEvent) => {
+    dispatch(updatePreferedLocation(e.target.value));
+  };
+  return (
+    <div className="selectors-container flex flex-row w-[100%]">
+      <div>
+        <Select
+          className="selector"
+          value={userPreferedLocationId}
+          label="Location"
+          onChange={handleChange}
+        >
+          <MenuItem value={"-1"} disabled>
+            Set location
+          </MenuItem>
+          <MenuItem value={"FPT-Q9"}>FPT Q9</MenuItem>
+          <MenuItem value={"NVH"}>NVH</MenuItem>
+        </Select>
+      </div>
+    </div>
+  );
+}
+
+export default PreferedLocationSelector;
