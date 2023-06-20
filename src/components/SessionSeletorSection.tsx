c8d7a33fac3403b85ea3df79428d5ac6bb1a7272
Gia Huy <70432101+lhgiahuy@users.noreply.github.com>
2023-06-20 13:56:40 +0700

    Homepage UI 2 (#16)
    * add theme and restyle homepage UI
    
    * add theme to homepage and remove tailwindcss
    
    * resolve conflict
    
    * resolve conflict
    
    * add menuPopper

--- src/components/SessionSeletorSection.tsx
+++ src/components/SessionSeletorSection.tsx
@@ -5,12 +5,19 @@ import {
   updateSessionId,
 } from "@/app/GlobalRedux/Features/orderSlice";
 import { useAppSelector } from "@/app/GlobalRedux/Features/userSlice";
-import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { useDispatch } from "react-redux";
+import "../style/SessionSelectorSection.scss";
 
 function SessionSeletorSection() {
   const sessionList = [
-    { id: "-1", title: "Set session" },
+    { id: "-1", title: " " },
     { id: "0", title: "9:15AM" },
     { id: "1", title: "11:45AM" },
     { id: "2", title: "2:45PM" },
@@ -33,34 +40,39 @@ function SessionSeletorSection() {
   return (
     <div className="selectors-container flex flex-row justify-center w-[100%]">
       <div>
-        <Select
-          className="selector"
-          value={locationId}
-          label="Location"
-          onChange={handleLocationIdChange}
-        >
-          <MenuItem value={"-1"} disabled>
-            Set location
-          </MenuItem>
-          <MenuItem value={"FPT-Q9"}>FPT Q9</MenuItem>
-          <MenuItem value={"NVH"}>NVH</MenuItem>
-        </Select>
-        <Select
-          className="selector"
-          value={sessionId}
-          label="Session"
-          onChange={handleSessionIdChange}
-        >
-          {sessionList.map((session) => (
-            <MenuItem
-              key={session.id}
-              value={session.id}
-              disabled={session.id == "-1"}
-            >
-              {session.title}
-            </MenuItem>
-          ))}
-        </Select>
+        <FormControl>
+          <InputLabel>Location</InputLabel>
+          <Select
+            className="selector"
+            value={locationId}
+            label="Location"
+            onChange={handleLocationIdChange}
+          >
+            <MenuItem value={"-1"} disabled></MenuItem>
+            <MenuItem value={"FPT-Q9"}>FPT Q9</MenuItem>
+
+            <MenuItem value={"NVH"}>NVH</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Session</InputLabel>
+          <Select
+            className="selector"
+            value={sessionId}
+            label="Session"
+            onChange={handleSessionIdChange}
+          >
+            {sessionList.map((session) => (
+              <MenuItem
+                key={session.id}
+                value={session.id}
+                disabled={session.id == "-1"}
+              >
+                {session.title}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </div>
     </div>
   );
