673c4fd5733daeea6d7aed1977b90aa3b531bcfb
Gia Huy <70432101+lhgiahuy@users.noreply.github.com>
2023-07-28 16:16:25 +0700

    Footer (#57)
    * style checkout page
    
    * add checkout UI
    
    * fix banner, add footer, fix order detail
    
    * hot fix footer

--- src/app/(customers)/(common-components)/Footer.tsx
+++ src/app/(customers)/(common-components)/Footer.tsx
@@ -9,55 +9,59 @@ function Footer() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <footer>
-          <div className="footer-container">
-            <div className="content">
-              <div className="logo-container">
-                <img
-                  className="logo"
-                  src="/navbar/COCCAN_white.png"
-                  alt="logo"
-                  width={80}
-                  height={80}
-                ></img>
-                <Typography variant="h4">COCCAN</Typography>
+        <div className="page-container">
+          <footer>
+            <div className="footer-container">
+              <div className="content">
+                <div className="logo-container">
+                  <img
+                    className="logo"
+                    src="/navbar/COCCAN_white.png"
+                    alt="logo"
+                    width={80}
+                    height={80}
+                  ></img>
+                  <Typography variant="h4">COCCAN</Typography>
+                </div>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "16px",
+                  }}
+                >
+                  <ul>
+                    <li className="social-icons">
+                      <a href="#">
+                        <Facebook></Facebook>
+                      </a>
+                    </li>
+                    <li className="social-icons">
+                      <a href="#">
+                        <Twitter></Twitter>
+                      </a>
+                    </li>
+                    <li className="social-icons">
+                      <a href="#">
+                        <Instagram></Instagram>
+                      </a>
+                    </li>
+                    <li className="social-icons">
+                      <a href="#">
+                        <Google></Google>
+                      </a>
+                    </li>
+                  </ul>
+                  <Typography variant="subtitle1">
+                    Made by F-Teamwork
+                  </Typography>
+                  <h6>PROJECT SWP391 - COCCAN</h6>
+                </Box>
               </div>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  gap: "16px",
-                }}
-              >
-                <ul>
-                  <li className="social-icons">
-                    <a href="#">
-                      <Facebook></Facebook>
-                    </a>
-                  </li>
-                  <li className="social-icons">
-                    <a href="#">
-                      <Twitter></Twitter>
-                    </a>
-                  </li>
-                  <li className="social-icons">
-                    <a href="#">
-                      <Instagram></Instagram>
-                    </a>
-                  </li>
-                  <li className="social-icons">
-                    <a href="#">
-                      <Google></Google>
-                    </a>
-                  </li>
-                </ul>
-                <Typography variant="subtitle1">Made by F-Teamwork</Typography>
-                <h6>PROJECT SWP391 - COCCAN</h6>
-              </Box>
             </div>
-          </div>
-        </footer>
+          </footer>
+        </div>
       </ThemeProvider>
     </>
   );
